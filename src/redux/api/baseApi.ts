7502701f3098013b6cf8773654832b687371ec36
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-12-28 19:09:10 +0600

    next.config.ts improved, redux-toolkit install and configured, and redux provider

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const url =
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+    ? process.env.NEXT_PUBLIC_BACKEND_URL
+    : "http://localhost:5000/api/v1";
+
+const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: url,
+    credentials: "include",
+  }),
+  tagTypes: [
+    "jobs",
+    "profile",
+    "user",
+    "applications",
+    "categories",
+    "company",
+  ],
+  endpoints: () => ({}),
+});
+
+export default baseApi;
