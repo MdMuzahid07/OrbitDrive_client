8c8ab3dfd597c4639deb250d949f90d89c5d9430
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-12-30 12:03:28 +0600

    minor change in baseAPI

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const url = "http://localhost:5000/api/v1/filesystem";
+const url =
+  process.env.NODE_ENV === "production"
+    ? `${process.env.NEXT_PUBLIC_API_URL}/filesystem`
+    : "http://localhost:5000/api/v1/filesystem";
 
 const baseApi = createApi({
   reducerPath: "baseApi",
